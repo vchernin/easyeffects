4c208b886e1b0c6235d66e74deb1fde92e4b5abc
root <110548574+easyeffects-bot@users.noreply.github.com>
2022-08-03 18:08:14 +0000

    Update json to 3.11.1

--- foo.sh
+++ foo.sh
@@ -1,16 +1,14 @@
-GIT_USER_NAME=$(git config user.name)
-GIT_USER_EMAIL=$(git config user.email)
-
 CWD=$(pwd)
-
-podman run --rm --privileged \
-    -v $HOME:$HOME:rslave \
-    -v $CWD:$CWD:rslave \
-    -w $CWD \
-    -e GIT_AUTHOR_NAME="bla" \
-    -e GIT_COMMITTER_NAME="foo" \
-    -e GIT_AUTHOR_EMAIL="foo@example.com" \
-    -e GIT_COMMITTER_EMAIL="foo@example.com" \
-    -e GITHUB_TOKEN="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" \
-    -it ghcr.io/vchernin/flatpak-external-data-checker:latest \
-    --always-fork --update util/flatpak/com.github.wwmm.easyeffects.json
+                
+        podman run --rm --privileged \
+          -v $HOME:$HOME:rslave \
+          -v $CWD:$CWD:rslave \
+          -w $CWD \
+          -e GIT_AUTHOR_NAME="EasyEffects Bot" \
+          -e GIT_COMMITTER_NAME="EasyEffects Bot" \
+          -e GIT_AUTHOR_EMAIL="110548574+easyeffects-bot@users.noreply.github.com" \
+          -e GIT_COMMITTER_EMAIL="110548574+easyeffects-bot@users.noreply.github.com" \
+          -e EMAIL="110548574+easyeffects-bot@users.noreply.github.com" \
+          -e GITHUB_TOKEN="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"  \
+          ghcr.io/flathub/flatpak-external-data-checker:latest \
+          --always-fork --update util/flatpak/com.github.wwmm.easyeffects.json
